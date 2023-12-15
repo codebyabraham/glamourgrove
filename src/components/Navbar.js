bbc0fc24321e31537c8bb2d61460da129d6e7d02
abeez07 <abrahamyusuf17@gmail.com>
2023-12-15 11:36:43 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
 <div id="menu-container">
   <ul class="nav justify-content-center">
   <nav class="navbar navbar-expand-lg navbar-light ">
-  <button class="navbar-toggler ml-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
